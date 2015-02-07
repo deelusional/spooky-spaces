889fc18a3ba3e7c4caaff7334138f532fa5a0161
deelusional <dtapmalai@gmail.com>
2015-02-07 23:26:12 +0000

    Added routes for new page redirec_to

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,11 +4,22 @@ class SpacesController < ApplicationController
     @spaces = Space.all
   end
   def index
-    @spaces = Space.order(:name).page(params[:page]).per(1)
+    @spaces = Space.order(:name).page(params[:page]).per(2)
   end
 
   def new
     @space = Space.new
+  end
+
+  def create
+    Space.create(space_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :description, :address)
   end
 
 end
