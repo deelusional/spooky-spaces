e6e8c41589e1626287cdb43f8826ff8cd37eb16c
deelusional <dtapmalai@gmail.com>
2015-02-07 22:48:25 +0000

    Added style to pagination

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+# Add pagination with kaminari
+gem 'kaminari'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,5 +3,7 @@ class SpacesController < ApplicationController
   def index
     @spaces = Space.all
   end
-
+  def index
+    @spaces = Space.order(:name).page(params[:page]).per(1)
+  end
 end
