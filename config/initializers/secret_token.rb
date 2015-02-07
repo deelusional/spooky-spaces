09052b6025e2489708d2b3548c2d8cb622382fbe
deelusional <dtapmalai@gmail.com>
2015-02-07 20:50:43 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpookySpaces::Application.config.secret_key_base = '4226b3f34a91168919eb3f2a15a28271b0d0f58f5977c28e7c0016afd0ffe3108a0692bd37b56ca8e623a314c292831a7de920411783e1f360ad618af592b209'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpookySpaces::Application.config.session_store :cookie_store, key: '_spooky_spaces_session'
