1e63d2d11be8cbdf7f504bf48f409bf0c201e81d
deelusional <dtapmalai@gmail.com>
2015-02-09 01:34:39 +0000

    Only logged in users can acces edit page

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit]
   
   def index
     @spaces = Space.all
