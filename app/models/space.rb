7b80c6430d85591712f53e5706a594e20a907a01
deelusional <dtapmalai@gmail.com>
2015-02-09 04:02:20 +0000

    Added validation to address/description and minimum length

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,6 @@
 class Space < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :minimum => 5 }
+	validates :address, :presence => true, :length => { :minimum => 10 }
+	validates :description, :presence => true, :length => { :minimum => 50 }
 end
