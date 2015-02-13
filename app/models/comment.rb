738ddc06c0bbf236d08c4987b6c6d1736dc535ae
deelusional <dtapmalai@gmail.com>
2015-02-13 04:32:50 +0000

    Added drop down for stars rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :space
+
+	RATINGS = {
+    'one star'    => '1_star',
+    'two stars'   => '2_stars',
+    'three stars' => '3_stars',
+    'four stars'  => '4_stars',
+    'five stars'  => '5_stars'
+  }
+
 end
