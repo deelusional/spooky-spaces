cb543019eda8717c72b57135867cadf9f117d63b
deelusional <dtapmalai@gmail.com>
2015-02-16 18:16:25 +0000

    Added NotificationMailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "no-reply@spooky_spacesapp.com"
+  default from: "no-reply@nomsterapp.com"
 
   def comment_added
   	mail(to: "dtapmalai@gmail.com", subject: "A comment has been added to your spooky space")
